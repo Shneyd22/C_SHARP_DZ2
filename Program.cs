630cf1175f9c4815b5a13cfef1d7e386ecf4374b
Sergei Schneidmiller <5797221@gmail.com>
2023-03-10 16:00:52 +0300

    dz2 ready

--- Program.cs
+++ Program.cs
@@ -16,22 +16,3 @@ while (index < 3)
         }
 }
 Console.WriteLine("Максимальное число: " + max);
-
-// Console.WriteLine("Введите первое число");
-// int num1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int num2 = Convert.ToInt32(Console.ReadLine());  
-// if (num1 > num2)
-// {
-//     Console.WriteLine("Максимальное число " + num1);
-//     Console.WriteLine("Минимальное число " + num2);
-// }
-// if (num1 < num2)
-// {
-//     Console.WriteLine("Максимальное число " + num2);
-//     Console.WriteLine("Минимальное число " + num1);
-// }
-// if (num1 == num2)
-// {
-//     Console.WriteLine("Числа равны");
-// }   
